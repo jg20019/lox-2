1d511610631ee4995a632644c3f34a21258a8561
John Gibson <jgibson@slingshotedu.com>
2025-05-13 22:49:17 -0400

    feat add error function for parse errors

--- src/errors.lisp
+++ src/errors.lisp
@@ -1,8 +1,10 @@
 (uiop:define-package lox.errors
   (:use #:cl)
+  (:import-from :lox.tokens :line :lexeme :token-type)
   (:export
    :*had-error*
    :lox-error
+   :report-parse-error
    :report))
 
 (in-package #:lox.errors)
@@ -11,6 +13,12 @@
 
 (defun lox-error (line message)
   (report line "" message))
+
+(defun report-parse-error (token message)
+  (with-slots (line lexeme token-type) token
+    (if (eql token-type :eof)
+	(report line " at end " message)
+	(report line (format nil " at '~a'" lexeme) message))))
 
 (defun report (line where message)
   (format
